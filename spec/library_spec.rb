a0965c9090c56eb352217cf01282202eb154a255
HoangNguyen58TH1 <hoangnv20081998@gmail.com>
2020-10-16 10:42:20 +0700

    watch & code follow tutorial -rspec book & library

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -36,5 +36,22 @@ describe "Library Object" do
       end
     end
   end
-  
+
+  it "returns all the books in a given category" do
+    @lib.get_books_in_category(:development).length.should == 2
+    # @lib.should have(2).get_books_in_category :development
+  end
+
+  it "accepts new books" do
+    @lib.add_book(Book.new("Designing for the Web", "Mark Boulton", :design))
+    @lib.get_book("Designing for the Web").should be_an_instance_of Book
+  end
+
+  it "saves the library" do
+    books = @lib.books.map { |book| book.title}
+    @lib.save "our_new_library.yml"
+    lib2 = Library.new "our_new_library.yml"
+    books2 = lib2.books.map { |book| book.title}
+    books.should eql books2
+  end
 end
